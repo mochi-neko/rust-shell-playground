cf22eaa6b766fa682e63cd2ee916c8dbc9486ea0
mochi-neko <t.o.e.4315@gmail.com>
2023-12-30 21:25:20 +0900

    Implement receiving command and arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Create input buffer
+    let mut input = String::new();
+
+    loop {
+        // Clear input buffer
+        input.clear();
+
+        // Read input from stdin
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        // Split input into elements
+        let elements: Vec<&str> = input
+            .split_whitespace()
+            .collect();
+
+        // If no elements, continue
+        if elements.is_empty() {
+            continue;
+        }
+
+        // Get command and args
+        let command = elements[0];
+        let args = &elements[1..];
+
+        // Print command and args
+        println!("Command: {:?}", command);
+        println!("Args: {:?}", args);
+    }
 }
