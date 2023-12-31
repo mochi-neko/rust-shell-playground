4c64ec8e6bc1643eedd153b83af747e2426a78f1
mochi-neko <t.o.e.4315@gmail.com>
2023-12-31 16:07:20 +0900

    Support path command

--- src/external_command.rs
+++ src/external_command.rs
@@ -7,6 +7,18 @@ pub(crate) struct ExternalCommand {
 
 impl ExternalCommand {
     pub(crate) fn find_command(command: &str) -> anyhow::Result<Option<Self>> {
+        // For relative and absolute paths, check if the path exists
+        if command.contains('/') {
+            let path = Path::new(command);
+
+            if path.exists() {
+                return Ok(Some(Self {
+                    path: path.to_path_buf(),
+                }));
+            }
+        }
+
+        // For each directory in PATH, check if the command exists
         for path_directory in env::var("PATH")?.split(':') {
             let path = Path::new(path_directory).join(command);
 
