eb0c7881fdd76cb7ba598c069050832c2770c3e8
mochi-neko <t.o.e.4315@gmail.com>
2024-01-04 10:21:02 +0900

    Implement shell script mode

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,37 @@ use external_command::ExternalCommand;
 use std::sync::{atomic::AtomicBool, Arc};
 
 fn main() -> anyhow::Result<()> {
+    // Deactivate Ctrl-C by signal handling
     let int = Arc::new(AtomicBool::new(false));
-    // Deactivate Ctrl-C
     signal_hook::flag::register(
         signal_hook::consts::SIGINT,
         Arc::clone(&int),
     )?;
 
+    // Judge mode by args
+    match std::env::args().nth(1) {
+        // $ cargo run -- script.sh
+        | Some(filename) => script_mode(filename)?,
+        // $ cargo run
+        | None => interactive_mode()?,
+    };
+
+    Ok(())
+}
+
+fn script_mode(filename: String) -> anyhow::Result<()> {
+    // Read file
+    let file = std::fs::read_to_string(filename)?;
+
+    // Interpret for each line
+    for line in file.lines() {
+        interpret_line(line.to_string())?;
+    }
+
+    Ok(())
+}
+
+fn interactive_mode() -> anyhow::Result<()> {
     loop {
         // Create input buffer
         let mut input = String::new();
@@ -21,51 +45,59 @@ fn main() -> anyhow::Result<()> {
         // Read input from stdin
         std::io::stdin().read_line(&mut input)?;
 
-        // Split input into elements
-        let elements: Vec<&str> = input
-            .split_whitespace()
-            .collect();
-
-        // If no elements, continue
-        if elements.is_empty() {
-            continue;
-        }
-
-        // Get command and args
-        let command = elements[0];
-        let args = &elements[1..];
-
-        // Parse and execute command
-        let command = find_command(command)?;
-
-        match command {
-            | Command::Builtin(builtin_command) => {
-                builtin_command
-                    .execute(args)
-                    .map_err(|error| {
-                        eprintln!(
-                            "Execute builtin command error: {:?}",
-                            error
-                        );
-                        error
-                    })?;
-            },
-            | Command::External(external_command) => {
-                external_command
-                    .execute(args)
-                    .map_err(|error| {
-                        eprintln!(
-                            "Execute external command error: {:?}",
-                            error
-                        );
-                        error
-                    })?;
-            },
-            | Command::NotFound(command) => {
-                eprintln!("Command not found: {}", command);
-            },
-        }
+        // Interpret line of input.
+        interpret_line(input)?;
     }
+}
+
+/// Interprets a line as command.
+fn interpret_line(line: String) -> anyhow::Result<()> {
+    // Split input into elements
+    let elements: Vec<&str> = line
+        .split_whitespace()
+        .collect();
+
+    // If no elements, continue
+    if elements.is_empty() {
+        return Ok(());
+    }
+
+    // Get command and args
+    let command = elements[0];
+    let args = &elements[1..];
+
+    // Parse and execute command
+    let command = find_command(command)?;
+
+    match command {
+        | Command::Builtin(builtin_command) => {
+            builtin_command
+                .execute(args)
+                .map_err(|error| {
+                    eprintln!(
+                        "Execute builtin command error: {:?}",
+                        error
+                    );
+                    error
+                })?;
+        },
+        | Command::External(external_command) => {
+            external_command
+                .execute(args)
+                .map_err(|error| {
+                    eprintln!(
+                        "Execute external command error: {:?}",
+                        error
+                    );
+                    error
+                })?;
+        },
+        | Command::NotFound(command) => {
+            eprintln!("Command not found: {}", command);
+        },
+    }
+
+    Ok(())
 }
 
 /// Commands in shell.
